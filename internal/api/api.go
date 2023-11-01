f229942f49a1ee79ae70eae77596c2332226a4ee
a-pillow <kaluhblee@gmail.com>
2023-11-01 16:05:20 -0500

    change log output to stdout

--- internal/api/api.go
+++ internal/api/api.go
@@ -28,6 +28,7 @@ func New(handlers *handlers.Handlers, log *slog.Logger) *Api {
 }
 
 func (a *Api) initializeRoutes() {
+	a.router.Use(middleware.Logger())
 	a.router.GET("/books", a.bookHandler.GetAllBooks)
 	a.router.GET("/books/:id", a.bookHandler.GetBookByIsbn)
 	a.router.POST("/books", a.bookHandler.CreateBook)
@@ -35,25 +36,10 @@ func (a *Api) initializeRoutes() {
 }
 
 func (a *Api) Listen() error {
-	f, err := os.OpenFile("logs.json", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		a.logger.Error("Failed to open logs file")
-	}
-	defer f.Close()
-
-	a.router.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-		Skipper: middleware.DefaultSkipper,
-		Format: `{"time":"${time_rfc3339_nano}","id":"${id}","remote_ip":"${remote_ip}",` +
-			`"host":"${host}","method":"${method}","uri":"${uri}","user_agent":"${user_agent}",` +
-			`"status":${status},"error":"${error}","latency":${latency},"latency_human":"${latency_human}"` +
-			`,"bytes_in":${bytes_in},"bytes_out":${bytes_out}}` + "\n",
-		CustomTimeFormat: "2006-01-02 15:04:05.00000",
-		Output:           f,
-	}))
 	a.initializeRoutes()
 
 	format := fmt.Sprintf(":%v", os.Getenv("PORT"))
-	err = a.router.Start(format)
+	err := a.router.Start(format)
 	if err != nil {
 		return err
 	}
