da2689a01ec62d25a6485ec6d63c2918b58b25bc
a-pillow <kaluhblee@gmail.com>
2023-11-01 15:55:59 -0500

    add file based logs

--- go.mod
+++ go.mod
@@ -3,13 +3,16 @@ module github.com/ca-lee-b/go-rest-boilerplate
 go 1.21.0
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gofor-little/env v1.0.16 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.4.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/echo/v4 v4.11.2 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -20,6 +23,7 @@ require (
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
 	gorm.io/driver/postgres v1.5.4 // indirect
 	gorm.io/gorm v1.25.5 // indirect
 )
--- internal/api/api.go
+++ internal/api/api.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/ca-lee-b/go-rest-boilerplate/internal/api/handlers"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 type Api struct {
@@ -34,10 +35,25 @@ func (a *Api) initializeRoutes() {
 }
 
 func (a *Api) Listen() error {
+	f, err := os.OpenFile("logs.json", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		a.logger.Error("Failed to open logs file")
+	}
+	defer f.Close()
+
+	a.router.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Skipper: middleware.DefaultSkipper,
+		Format: `{"time":"${time_rfc3339_nano}","id":"${id}","remote_ip":"${remote_ip}",` +
+			`"host":"${host}","method":"${method}","uri":"${uri}","user_agent":"${user_agent}",` +
+			`"status":${status},"error":"${error}","latency":${latency},"latency_human":"${latency_human}"` +
+			`,"bytes_in":${bytes_in},"bytes_out":${bytes_out}}` + "\n",
+		CustomTimeFormat: "2006-01-02 15:04:05.00000",
+		Output:           f,
+	}))
 	a.initializeRoutes()
 
 	format := fmt.Sprintf(":%v", os.Getenv("PORT"))
-	err := a.router.Start(format)
+	err = a.router.Start(format)
 	if err != nil {
 		return err
 	}
